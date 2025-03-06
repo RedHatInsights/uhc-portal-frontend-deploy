daff39fa2506c2fe8653cdb15a853e08ceedcddc
OCM Team <ocm-feedback@redhat.com>
2025-03-06 12:27:55 +0000

    Update to 842d6a7
    Revert release fixes 20250306 (#228)

--- bundle.628.68681bcb9a732202.js
+++ bundle.628.68681bcb9a732202.js
@@ -1,3 +1,3 @@
 "use strict";(self.webpackChunkopenshift=self.webpackChunkopenshift||[]).push([[628],{45628:(e,s,h)=>{h.r(s)}}]);
-!function(){try{var e="undefined"!=typeof window?window:"undefined"!=typeof global?global:"undefined"!=typeof globalThis?globalThis:"undefined"!=typeof self?self:{},n=(new e.Error).stack;n&&(e._sentryDebugIds=e._sentryDebugIds||{},e._sentryDebugIds[n]="0a12ba94-dbee-5862-9955-4f0b25e61525")}catch(e){}}();
+!function(){try{var e="undefined"!=typeof window?window:"undefined"!=typeof global?global:"undefined"!=typeof self?self:{},n=(new Error).stack;n&&(e._sentryDebugIds=e._sentryDebugIds||{},e._sentryDebugIds[n]="0a12ba94-dbee-5862-9955-4f0b25e61525")}catch(e){}}();
 //# debugId=0a12ba94-dbee-5862-9955-4f0b25e61525
